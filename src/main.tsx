ceef176b77a861bcc07a29fd270a6661ea68fc26
besfeng23 <136943241+besfeng23@users.noreply.github.com>
2026-06-11 08:02:53 -0700

    style: import phase 3 lite css

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './phase1.css';
+import './phase3-lite.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
