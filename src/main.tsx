79e369cf9de2d9270484c09cd894936e909fee14
besfeng23 <136943241+besfeng23@users.noreply.github.com>
2026-06-12 02:21:32 -0700

    revert: remove blocked runtime fallback wiring

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ErrorBoundary } from './components/ErrorBoundary';
 import './styles.css';
 import './phase1.css';
 import './phase3-lite.css';
@@ -9,10 +8,4 @@ import './phase4-lite.css';
 import './phase5-lite.css';
 import './phase8-lite.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
