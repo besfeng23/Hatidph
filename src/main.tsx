c635c621fd0402ee8fe31ea1b39213120d78f6ff
besfeng23 <136943241+besfeng23@users.noreply.github.com>
2026-06-11 19:53:03 -0700

    style: import phase 8A polish

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import './phase1.css';
 import './phase3-lite.css';
 import './phase4-lite.css';
 import './phase5-lite.css';
+import './phase8-lite.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
